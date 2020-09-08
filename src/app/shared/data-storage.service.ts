0d5a6194ee4414dc781d6446a2d836a9b0ae5c5e
eleex <eleex1988@gmail.com>
2020-09-08 14:24:04 +0300

    store data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from './../recipes/recipe.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http
+      .put('https://ng-recipe-book-e3a68.firebaseio.com/recipes.json', recipes)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
