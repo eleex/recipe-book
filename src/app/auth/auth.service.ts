970593af636ba525e8c54a15c9776f968da6fbbe
eleex <eleex1988@gmail.com>
2020-09-11 10:46:54 +0300

    creating user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,38 +1,74 @@
-import { AuthResponseData } from './models/auth-response-data.model';
-import { AuthRequestData } from './models/auth-request-data';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, throwError, Subject } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { AuthRequestData } from './models/auth-request-data';
+import { AuthResponseData } from './models/auth-response-data.model';
+import { User } from './models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   apiKey = 'AIzaSyCO1CEnYSDHJFvNLfLpn5tfRJHY7D3O0DM';
+  user = new Subject<User>();
 
   constructor(private http: HttpClient) {}
 
-  signup(user: AuthRequestData): Observable<AuthResponseData> {
-    user.returnSecureToken = true;
+  signup(userData: AuthRequestData): Observable<AuthResponseData> {
+    userData.returnSecureToken = true;
 
     return this.http
       .post<AuthResponseData>(
-        `https://identitytoolkit.googleapis.co/v1/accounts:signUp?key=${this.apiKey}`,
-        user
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.apiKey}`,
+        userData
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
-  login(user: AuthRequestData): Observable<AuthResponseData> {
-    user.returnSecureToken = true;
+  login(userData: AuthRequestData): Observable<AuthResponseData> {
+    userData.returnSecureToken = true;
 
     return this.http
       .post<AuthResponseData>(
         `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apiKey}`,
-        user
+        userData
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ): void {
+    const expirationDate = new Date(Date.now() + expiresIn * 1000);
+
+    const user = new User(email, userId, token, expirationDate);
+
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse): Observable<never> {
