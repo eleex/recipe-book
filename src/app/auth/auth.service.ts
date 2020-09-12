a9d4656b1a46190e630205cf2e4a60a46501fb4c
eleex <eleex1988@gmail.com>
2020-09-12 17:03:24 +0300

    auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -59,6 +59,30 @@ export class AuthService {
       );
   }
 
+  autoLogin(): void {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout(): void {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -71,10 +95,10 @@ export class AuthService {
     expiresIn: number
   ): void {
     const expirationDate = new Date(Date.now() + expiresIn * 1000);
-
     const user = new User(email, userId, token, expirationDate);
 
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse): Observable<never> {
